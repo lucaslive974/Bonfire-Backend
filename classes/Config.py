f10a9e7a62c4eca433c01e323004f24ad4ca84cd
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-06-03 11:48:22 -0300

    refactor(config): add explicit type annotations to Config attributes in classes/Config.py

--- classes/Config.py
+++ classes/Config.py
@@ -1,41 +1,57 @@
-from os import getenv 
+from os import getenv
 from exceptions.CustomExceptions import ErrMissingRequiredEnv
 
-env_default_variables = { 
-    "DB_DRIVER": "mysql",
-    "DB_HOST": "bonfire-db", 
-    "DB_PORT": "3306",
-    "DB_NAME": "bonfire",
-    "DB_USER": "bonfire", 
-    "DB_PASSWORD": None,
-
-    # auth
-    "KEYCLOAK_ISSUER": "http://keycloak:8080",
-    "KEYCLOAK_CLIENT_ID": "bonfire",
-    "KEYCLOAK_CLIENT_SECRET": None,
-    "KEYCLOAK_REALM_NAME": None,
-}
 
 class Config:
+    DB_DRIVER: str = "mysql"
+    DB_HOST: str = "bonfire-db"
+    DB_PORT: str = "3306"
+    DB_NAME: str = "bonfire"
+    DB_USER: str = "bonfire"
+    DB_PASSWORD: str | None = None
+
+    # auth
+    KEYCLOAK_ISSUER: str = "http://keycloak:8080"
+    KEYCLOAK_CLIENT_ID: str = "bonfire"
+    KEYCLOAK_CLIENT_SECRET: str | None = None
+    KEYCLOAK_REALM_NAME: str | None = None
+
     def __init__(self):
-        self.envs: dict[str, str] = {}
-        for env in env_default_variables:
-            value = getenv(env, env_default_variables[env]) 
+        for env in self:
+            value = getenv(env, self[env])
             if not value:
                 raise ErrMissingRequiredEnv("ERROR::Missing required env: " + env)
 
-            self.envs[env] = value
+            self[env] = value
+
+    def __iter__(self):
+        for attr in self.__class__.__dict__:
+            if not attr.startswith("_"):
+                val = getattr(self, attr)
+                if not callable(val):
+                    yield attr
+
+    def __getitem__(self, item: str):
+        try:
+            val = getattr(self, item)
+            if callable(val):
+                raise AttributeError
+            return val
+        except AttributeError:
+            raise KeyError(f"Variável de configuração '{item}' não encontrada.")
+
+    def __setitem__(self, key: str, value):
+        try:
+            if not hasattr(self, key) or key.startswith("_"):
+                raise AttributeError
+
+            if callable(getattr(self, key)):
+                raise AttributeError
+
+            setattr(self, key, value)
+
+        except AttributeError:
+            raise KeyError(f"Não é permitido definir/alterar a variável '{key}'.")
+
 
 config = Config()
-
-#  Example
-#  
-#  "driver": "mysql",
-#  "host": "192.168.0.11",
-#  "port": "3306",
-#  "database": "bonfire",
-#  "user": "bonfire",
-#   "password": "strong-password"
-#   
-# 
-
