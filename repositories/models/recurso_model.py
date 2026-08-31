0ecde357cab892d928869ad1c4b5cc82128b6379
lucaslive974 <lucasribeirolima974@gmail.com>
2026-08-30 21:32:22 -0300

    [fix]: remove ID field and set NUM_AI and NUM_ATA as primary keys in Recurso models

--- repositories/models/recurso_model.py
+++ repositories/models/recurso_model.py
@@ -6,9 +6,8 @@ from repositories.models.Base import Base
 class RecursoPrimeiraInstanciaModel(Base):
     __tablename__ = "recurso_primeira_instancia"
 
-    ID = Column(Integer, primary_key=True, autoincrement=True)
-    NUM_AI = Column(String(15), unique=True, nullable=True)
-    NUM_ATA = Column(Integer, nullable=True)
+    NUM_AI = Column(String(15), primary_key=True, nullable=False)
+    NUM_ATA = Column(Integer, primary_key=True, nullable=False)
     NUM_RECURSO = Column(String(15), nullable=True)
     NOM_CONC = Column(String(100), nullable=True)
     RESULTADO = Column(Boolean, nullable=False)
@@ -22,8 +21,7 @@ class RecursoPrimeiraInstanciaModel(Base):
 class RecursoSegundaInstanciaModel(Base):
     __tablename__ = "recurso_segunda_instancia"
 
-    ID = Column(Integer, primary_key=True, autoincrement=True)
-    NUM_AI = Column(String(15), unique=True, nullable=True)
+    NUM_AI = Column(String(15), primary_key=True, nullable=False)
     NUM_RECURSO = Column(String(15), nullable=True)
     NOM_CONC = Column(String(100), nullable=True)
     RESULTADO = Column(Boolean, nullable=False)
